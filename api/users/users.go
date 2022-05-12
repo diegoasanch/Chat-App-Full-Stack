5da74a7017eeade56b26980054f120903c427db2
Diego Sanchez <diegoasp18@gmail.com>
2022-05-12 02:43:17 -0300

    Add user login

--- api/users/users.go
+++ api/users/users.go
@@ -2,14 +2,18 @@ package users
 
 import (
 	"chat-app/api/db"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func UserRoutes(router *gin.RouterGroup) {
-	router.POST("/create", createUser)
+	router.POST("/signup", createUser)
+	router.POST("/login", login)
 }
 
 type CreateUserBody struct {
@@ -42,4 +46,47 @@ func createUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{ "status": "ok", "message": "User created", "userId":  user.BaseDbModel.ID  })
+}
+
+type LoginBody struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+func login(c *gin.Context) {
+	body := LoginBody{}
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{ "status": "error" })
+		return
+	}
+	if body.Email == "" || body.Password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{ "status": "error", "message": "Invalid payload" })
+		return
+	}
+	var user db.User
+	result := db.DB.Where("email = ?", body.Email).First(&user)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{ "status": "error", "message": result.Error.Error() })
+		return
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{ "status": "error", "message": "Invalid credentials" })
+		return
+	}
+
+	hmacSampleSecret := []byte("secretCode")
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId": user.BaseDbModel.ID,
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(hmacSampleSecret)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{ "status": "error", "message": "Error signing token" })
+		fmt.Println(err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{ "status": "ok", "message": "User logged in", "token": tokenString })
 }
